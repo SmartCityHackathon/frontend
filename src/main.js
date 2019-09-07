035a66efb80549e9c42b4bca0f34055bf72b6a02
Pavol Hejny <me@pavolhejny.com>
2019-09-07 19:46:51 +0200

    Prettier

--- src/main.js
+++ src/main.js
@@ -1,55 +1,34 @@
-
-
-
-
-class Calendar{
-
- changeDate(element,moving){
-    if(moving==='GOING'){
-        element.classList.add('not')
-    }else{
-        element.classList.remove('not')
-    }
- }
-
-
-
-
-
-
-
- constructor(){
-
-    this.isMoving = false;
-
-for(const element of document.querySelectorAll('td')){
-    element.addEventListener('pointerdown',()=>{
-    
-        this.isMoving = element.classList.contains('not')?'NOT':'GOING';
-        changeDate(element,this.isMoving)
-    
-    });
-
-
-    for(const eventType of ['pointerleave','pointerenter']){
-    element.addEventListener(eventType,()=>{
-        if(this.isMoving){
-            changeDate(element,this.isMoving);
+class Calendar {
+    changeDate(element, moving) {
+        if (moving === 'GOING') {
+            element.classList.add('not');
+        } else {
+            element.classList.remove('not');
         }
-    });
     }
 
-}
+    constructor() {
+        this.isMoving = false;
 
+        for (const element of document.querySelectorAll('td')) {
+            element.addEventListener('pointerdown', () => {
+                this.isMoving = element.classList.contains('not')
+                    ? 'NOT'
+                    : 'GOING';
+                changeDate(element, this.isMoving);
+            });
 
-document.querySelector('body').addEventListener('pointerup',()=>{
+            for (const eventType of ['pointerleave', 'pointerenter']) {
+                element.addEventListener(eventType, () => {
+                    if (this.isMoving) {
+                        changeDate(element, this.isMoving);
+                    }
+                });
+            }
+        }
 
-    this.isMoving = 
-    false;
-});
-
- }
-
-
-
+        document.querySelector('body').addEventListener('pointerup', () => {
+            this.isMoving = false;
+        });
+    }
 }
