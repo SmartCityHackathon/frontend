0ecf0bfd01474eea5db742fb6145cc635ba2b5de
Barbora Vobořilová <bara.voboril@seznam.cz>
2019-09-07 13:52:40 +0200

    h

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-for(const element of document.querySelectorAll('td')){
-
-    element.addEventListener('click',()=>{
-        if(element.classList.contains('not')){
-            element.classList.remove('not')
-        }else{
-            element.classList.add('not')
-        }
-    });
 
 
+function changeDate(element){
+    
+    if(element.classList.contains('not')){
+        element.classList.remove('not')
+    }else{
+        element.classList.add('not')
+    }
 
 }
+
+let moving = false;
+
+for(const element of document.querySelectorAll('td')){
+    element.addEventListener('click',changeDate.bind(this,element));
+    element.addEventListener('pointerenter',()=>{
+        if(moving){
+            changeDate(element);
+        }
+    });
+}
+
+document.querySelector('table').addEventListener('pointerdown',()=>{
+    moving = true;
+});
+
+
+document.querySelector('table').addEventListener('pointerup',()=>{
+    moving = false;
+});
+
